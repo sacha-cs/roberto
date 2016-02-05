37a24b1cc4cad407aa9440777808f27a9236c246
Mr. Pi <pi@pi.pi>
2016-02-05 12:25:11 +0000

    Add changes on pi

--- plotlog.py
+++ plotlog.py
@@ -1,18 +1,13 @@
 import matplotlib.pyplot as plt
 
-def plot(data, timeCol, motorCol, refCol):
-    times = [row[timeCol] for row in data]
-    motor = [row[motorCol] - data[0][motorCol] for row in data]
-    ref = [row[refCol] - data[0][motorCol] for row in data]
-    error = [motor[i] - ref[i] for i in range(len(motor))]
-
-    plt.plot(times, motor, '', times, ref, '', times, error)
-    plt.show()
-
 data = []
 
 with open('logfile') as f:
         data = [[float(num) for num in line.strip().split('\t')] for line in f.readlines()]
 
-plot(data, 0, 1, 2)
-plot(data, 0, 3, 4)
+times = [row[0] for row in data]
+motor1 = [row[1] for row in data]
+ref1 = [row[2] for row in data]
+
+plt.plot(times, motor1, '', times, ref1)
+plt.show()
