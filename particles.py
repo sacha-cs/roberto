a4198f786b0e89ca937641805d20b50cb03ac3df
Mr. Pi <pi@pi.pi>
2016-02-14 15:58:09 +0000

    Particles almost working.

--- particles.py
+++ particles.py
@@ -1,46 +1,57 @@
 import Practical2.robot_utils as ru
 import random
+import math
 
 SCALE_FACTOR = 15
 NUMBER_OF_PARTICLES = 100
 
 # Parameters for random variables
 MEAN = 0
-SIGMA_X = 10
-SIGMA_Y = 10
-SIGMA_THETA = 1
+SIGMA_E = 0.5
+SIGMA_F = 0.1
+SIGMA_G = 0.1
 
 # Offset for drawing particles and path
-OFFSET = 50  # pixels
+OFFSET = 100  # pixels
 
 ru.start()
 
-# Initialise coordinates
-x = OFFSET
-y = OFFSET
+# Initialise coordinates and angle
+x = 0
+y = 0
 theta = 0
 
-particles = [(x,y,0)]*NUMBER_OF_PARTICLES
+# Initialise particles
+particles = [(0,0,0)]*NUMBER_OF_PARTICLES
 
-def getRandomX(x):
-    dx = random.gauss(MEAN, SIGMA_X)
-    return x + dx
+def getNoiseTermE():
+    return random.gauss(MEAN, SIGMA_E)
 
-def getRandomY(y):
-    dy = random.gauss(MEAN, SIGMA_Y)
-    return y + dy
+def getNoiseTermF():
+    return random.gauss(MEAN, SIGMA_F)
 
-def getRandomTheta(theta):
-    dtheta = random.gauss(MEAN, SIGMA_THETA)
-    return theta + dtheta
+def getNoiseTermG():
+    return random.gauss(MEAN, SIGMA_G)
 
 def drawParticles(particles):
     print 'drawParticles:' + str([(OFFSET + SCALE_FACTOR * x, OFFSET + SCALE_FACTOR * y, theta) for (x,y,theta) in particles]) 
 
 def drawLine(xstart, ystart, x, y):
-    xstart, ystart, x, y = line
-    print 'drawLine:' + str((OFFSET + xstart, OFFSET + ystart, OFFSET + x, OFFSET + y))
+    print 'drawLine:' + str((OFFSET + SCALE_FACTOR * xstart, OFFSET + SCALE_FACTOR * ystart, OFFSET + SCALE_FACTOR * x, OFFSET + SCALE_FACTOR * y))
 
+def updateAfterStraightLine(x, y, theta, distance):
+    xNew = x + ( distance + getNoiseTermE() ) * math.cos(theta)
+    yNew = y + ( distance + getNoiseTermE() ) * math.sin(theta)
+    thetaNew = theta + getNoiseTermF()
+    return (xNew, yNew, thetaNew)
+
+def updateAfterRotation(x, y, theta, alpha):
+    xNew = x
+    yNew = y
+    thetaNew = theta + alpha + getNoiseTermG()
+    return (xNew, yNew, thetaNew)
+
+theta_change = 90  # turn 90 degrees each time
 for i in range(4):
 
    if (i == 0):
@@ -62,19 +73,18 @@ for i in range(4):
        # Update position
        xstart = x
        ystart = y
-       x += x_change * 10
+       x += x_change * 10 
        y += y_change * 10
 
        # Update particles
-       particles = [(getRandomX(x0), getRandomY(y0), getRandomTheta(theta0)) for (x0,y0,theta0) in particles]
+       particles = [updateAfterStraightLine(x0,y0,theta0,10*(x_change+y_change)) for (x0,y0,theta0) in particles]
 
        # Draw lines and particles
        drawLine(xstart, ystart, x, y)
        drawParticles(particles)
-       
 
-
-
-   ru.turnLeft(90)
+   ru.turnLeft(theta_change)
+   theta += theta_change  # turned 90 degrees
+   particles = [updateAfterRotation(x0,y0,theta0,theta_change) for (x0,y0,theta0) in particles]  # update particles
 
 ru.done()
