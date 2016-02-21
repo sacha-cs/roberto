6d454a7dc758daf5e19394b0ae54fa25adf4f62e
Roberto <roberto@pi.com>
2016-02-21 16:25:19 +0000

    Tuning roberto

--- Practical2/robot_utils.py
+++ Practical2/robot_utils.py
@@ -62,14 +62,14 @@ def setupMotors(control=CONTROL_ANGLE):
     interface.setMotorAngleControllerParameters(motors[0],motorParams)
     interface.setMotorAngleControllerParameters(motors[1],motorParams)
 
-RADIANS_40CM = 11.7
+RADIANS_40CM = 14.35
 def move(distance, verbose=False, wait=True):
     radians = distance / 40.0 * RADIANS_40CM
     interface.increaseMotorAngleReferences(motors,[radians,radians])
     if (wait):
         waitUntilStopped(verbose)
 
-RADIANS_90DEG = 3.8
+RADIANS_90DEG = 4.8
 def turnRight(deg, verbose=False, wait=True):
     radians = deg/90.0 * RADIANS_90DEG
     interface.increaseMotorAngleReferences(motors,[radians,-radians])
--- Practical2/square.py
+++ Practical2/square.py
@@ -2,17 +2,13 @@ import robot_utils as ru
 
 ru.start()
 
-ru.interface.startLogging('straightlinelog')
-
 ru.move(40)
-#ru.turnLeft(90)
-#ru.move(40)
-#ru.turnLeft(90)
-#ru.move(40)
-#ru.turnLeft(90)
-#ru.move(40)
-#ru.turnLeft(90)
-
-ru.interface.stopLogging()
+ru.turnLeft(90)
+ru.move(40)
+ru.turnLeft(90)
+ru.move(40)
+ru.turnLeft(90)
+ru.move(40)
+ru.turnLeft(90)
 
 ru.done()
